a8564bc182156c12e1918d1ce99c622fa710a6ec
Michael Cheng <mcheng@us.ibm.com>
2020-02-21 15:07:17 -0600

    update CRDs

---
+++ pkg/apis/events/v1alpha1/eventmediations_types.go
@@ -0,0 +1,83 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// EventMediationsSpec defines the desired state of EventMediations
+type EventMediationsSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+    Mediations []EventMediations `json:"mediations"`
+}
+
+type EventMediations struct {
+    Mediation *EventMediationImpl `json:"mediation"`
+    Function *EventFunctionImpl `json:"function"`
+}
+
+/* Valid combinations are:
+  1) assignment
+  2) if and assignment
+  3) if and body
+  4) switch
+  5) if and switch
+  TBD: switch and default
+*/
+type EventStatement struct {
+    If    *string `json:"if"`
+    Assign  *string `json:"="`
+    Switch  *[]EventStatement `json:"switch",omitempty`
+    Body *[]EventStatement `json:"body",omitempty`
+}
+
+
+type  EventFunctionImpl struct {
+    Name string `json:"name"`
+    Input string `json:"input"`
+    Output string `json:"output"`
+    Body []EventStatement `json:"body"`
+}
+
+type EventMediationImpl  struct {
+    Name string `json:"name"`
+    SendTo []string `json:"sendTo",omitempty`
+    Body []EventStatement `json:"body",omitempty`
+}
+
+// EventMediationsStatus defines the observed state of EventMediations
+type EventMediationsStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// EventMediations is the Schema for the eventmediations API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=eventmediations,scope=Namespaced
+type EventMediations struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EventMediationsSpec   `json:"spec,omitempty"`
+	Status EventMediationsStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// EventMediationsList contains a list of EventMediations
+type EventMediationsList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EventMediations `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EventMediations{}, &EventMediationsList{})
+}
--- pkg/apis/events/v1alpha1/eventmediator_types.go
+++ pkg/apis/events/v1alpha1/eventmediator_types.go
@@ -12,44 +12,20 @@ type EventMediatorSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-    Code []EventImplementation `json:"code",omitempty`
-}
-
-type EventImplementation struct {
-    Mediation *EventMediation `json:"mediation",omitempty`
-    Function *EventFunction  `json:"function",omitempty`
-}
-
-type EventFunction struct {
-    Name string `json:"name"`
-    Input string `json:"input"`
-    Output string `json:"output"`
-    Body []EventStatement `json:"body"`
+    Listeners *[]EventListenerConfig `json:"listeners",omitempty` // default is no listener
+    importMediations  *[]string `json:"importMediations",omitempty` // default is to import everything unless code is specified
+    Mediations *[]EventMediations `json:"mediations"`
 }
 
 
-type EventMediation struct {
-    Name string `json:"name",omitempty`
-    SubscribeFrom [] string `json:"subscribeFrom",omitempty`
-    SendTo [] string `json:"sendTo",omitempty`
-    Body [] EventStatement `json:"body"`
+type EventListenerConfig struct {
+    Name string  `json:"name",omitempty`  // name of the listener configuration. Default is the name of the MediatorSpec
+    Mediations []string `json:"mediations"` // if not specified, applies to all mediations
+    HttpPort    int         `json:"httpPort",omitempty`
+    HttpsPort   int         `json:"httpsPort",omitempty`
+    CreateService bool `json:"createService",omitempty`
+    CreateRoute bool      `json:"createRoute",omitempty`
 }
-
-/* Valid combinations are:
-  1) assignment
-  2) if and assignment
-  3) if and body
-  4) switch
-  5) if and switch
-  TBD: switch and default
-*/
-type EventStatement struct {
-    If    *string `json:"if"`
-    Assign  *string `json:"="`
-    Switch  *[]EventStatement `json:"switch",omitempty`
-    Body *[]EventStatement `json:"body",omitempty`
-}
-
 
 // EventMediatorStatus defines the observed state of EventMediator
 type EventMediatorStatus struct {
